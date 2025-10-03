0de3f31f83ed22be388d66bb78706ea55c476d24
DonutCables <donutcatproductions@gmail.com>
2025-10-03 04:02:02 -0400

    tentative hv20t soundcard library

--- buckets_networked/lib/audio_commands-hv20t.py
+++ buckets_networked/lib/audio_commands-hv20t.py
@@ -9,39 +9,53 @@ class Sound_Control:
 
     def __init__(self, audio: UART):
         self.audio_out = audio
-
-    cmd_base = bytearray([0x7E, 0xFF, 0x06, 0x00, 0x00, 0x00, 0x00, 0xEF])
+        self.cmd_base = bytearray(
+            [
+                0xAA,
+            ]
+        )
 
     def play_track(self, track: int):
         """Plays a track based on the numerical name"""
-        play_track = self.cmd_base
-        play_track[3] = 0x03
-        play_track[6] = int(hex(track))
+        play_track = self.cmd_base.copy()
+        play_track.append(0x07)
+        play_track.append(0x02)
+        play_track.append(int(hex(track)))
+        play_track.append(0x00)
+        sum = (0x07 + 0x02 + int(hex(track)) + 0x00) & 0xFF
+        play_track.append(sum)
         self.audio_out.write(play_track)
 
     def vol_up(self):
         """Increments volume by one"""
-        vol_up = self.cmd_base
-        vol_up[3] = 0x04
+        vol_up = self.cmd_base.copy()
+        vol_up.append(0x14)
+        vol_up.append(0x00)
+        vol_up.append(0xBE)
         self.audio_out.write(vol_up)
 
     def vol_down(self):
         """Decrements volume by one"""
-        vol_down = self.cmd_base
-        vol_down[3] = 0x05
+        vol_down = self.cmd_base.copy()
+        vol_down.append(0x15)
+        vol_down.append(0x00)
+        vol_down.append(0xBF)
         self.audio_out.write(vol_down)
 
     def set_vol(self, level=30):
         """Sets volume level from 0-30"""
-        set_vol = self.cmd_base
-        set_vol[3] = 0x06
-        set_vol[6] = int(hex(level))
+        set_vol = self.cmd_base.copy()
+        set_vol.append(0x13)
+        set_vol.append(0x01)
+        set_vol.append(int(hex(level)))
+        sum = (0x13 + 0x01 + int(hex(level))) & 0xFF
+        set_vol.append(sum)
         self.audio_out.write(set_vol)
 
-    def play_at_vol(self, track=1, vol=30):
-        """Plays a track at a set volume"""
+    """def play_at_vol(self, track=1, vol=30):
+        "Plays a track at a set volume"
         play_at_vol = self.cmd_base
         play_at_vol[3] = 0x22
         play_at_vol[5] = int(hex(vol))
         play_at_vol[6] = int(hex(track))
-        self.audio_out.write(play_at_vol)
+        self.audio_out.write(play_at_vol)"""
